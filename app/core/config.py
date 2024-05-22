04f5bb948861a18192325884d8ef6e3edd399ef6
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 23:02:09 +0300

    Config and database works

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import BaseModel, PostgresDsn
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -16,7 +16,14 @@ class ApiPrefixSettings(BaseModel):
 
 
 class DbSettings(BaseModel):
-    url: PostgresDsn
+    # model_config = SettingsConfigDict(
+    #     env_file='.env',
+    #     case_sensitive=False,
+    #     env_nested_delimiter="__",
+    #     env_prefix="FASTAPI__"
+    # )
+    # url: PostgresDsn
+    url: str = 'postgresql+asyncpg://user:password@localhost:7777/shop'
     echo: bool = True
     echo_pool: bool = False
     pool_size: int = 50
@@ -26,7 +33,8 @@ class DbSettings(BaseModel):
 class Settings(BaseSettings):
     run: RunSettings = RunSettings()
     api: ApiPrefixSettings = ApiPrefixSettings()
-    db: DbSettings
+    db: DbSettings = DbSettings()
 
 
 settings = Settings()
+print(settings.dict())
