dee044977a969e74f43c4d998e7ed14e8d5ada9a
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 20:39:44 +0300

    Create User model

---
+++ app/models/user.py
@@ -0,0 +1,8 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String
+
+from .base import Base
+
+
+class User(Base):
+    username: Mapped[int] = mapped_column(String(40), unique=True)
