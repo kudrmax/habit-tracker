702aea300dfcb82afbcdfc8e62121c1b0811a830
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-22 22:09:15 +0300

    Create new db_helper

--- app/core/config.py
+++ app/core/config.py
@@ -1,20 +1,32 @@
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-DB_PATH = BASE_DIR / 'db.sqlite3'
+
+
+class RunSettings(BaseModel):
+    host: str = 'localhost'
+    port: int = 5432
+
+
+class ApiPrefixSettings(BaseModel):
+    prefix: str = 'api'
 
 
 class DbSettings(BaseModel):
-    url: str = f"sqlite+aiosqlite:///{DB_PATH}"
+    url: PostgresDsn
     echo: bool = True
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
 
 
 class Settings(BaseSettings):
-    api_prefix: str = "/api"
-    db: DbSettings = DbSettings()
+    run: RunSettings = RunSettings()
+    api: ApiPrefixSettings = ApiPrefixSettings()
+    db: DbSettings
 
 
 settings = Settings()
--- app/models/db_helper.py
+++ app/models/db_helper.py
@@ -1,42 +1,68 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import (
     create_async_engine,
+    AsyncEngine,
     async_sessionmaker,
-    async_scoped_session,
     AsyncSession,
 )
-from asyncio import current_task
 
 from core.config import settings
 
 
-class DataBaseHelper:
-    def __init__(self, url: str, echo: bool = False):
-        self.engine = create_async_engine(
+class DatabaseHelper:
+    def __init__(
+            self,
+            url: str,
+            echo: bool = False,
+            echo_pool: bool = False,
+            pool_size: int = 5,
+            max_overflow: int = 10,
+    ) -> None:
+        """
+        Parameters
+        ----------
+        url: url for database connection
+        echo: выводить ли дополнительную информацию
+        echo_pool: выводить ли дополнительную информацию
+        pool_size
+        max_overflow
+        """
+        # асинхронный движок
+        self.engine: AsyncEngine = create_async_engine(
             url=url,
             echo=echo,
+            echo_pool=echo_pool,
+            pool_size=pool_size,
+            max_overflow=max_overflow,
         )
-        self.session_factory = async_sessionmaker(
+
+        # фабрика сессий
+        self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
             bind=self.engine,
-            autocommit=False,
             autoflush=False,
+            autocommit=False,
             expire_on_commit=False,
         )
 
-    def get_scoped_session(self):
-        session = async_scoped_session(
-            session_factory=self.session_factory, scopefunc=current_task  # @todo ???
-        )
-        return session
+    async def dispose(self) -> None:
+        """
+        Завершение работы с движком
+        """
+        await self.engine.dispose()
 
-    async def session_dependency(self) -> AsyncSession:
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        """
+        Метод для получения сессии
+        """
         async with self.session_factory() as session:
-            yield session
-            await session.close()
-
-    async def scoped_session_dependency(self) -> AsyncSession:
-        session = self.get_scoped_session()
-        yield session
-        await session.close()
+            yield session  # @todo почему yield и как это вообще работает?
 
 
-db_helper = DataBaseHelper(url=settings.db.url, echo=settings.db.echo)
+db_helper = DatabaseHelper(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow,
+)
