5cd80b3f05c7795f47b724f05b652e108eddf0ed
kudr.max <mdkudryashov@edu.hse.ru>
2024-05-23 15:12:54 +0300

    Add user_create in API

--- app/api/user.py
+++ app/api/user.py
@@ -1,15 +1,27 @@
+from typing import Annotated
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from crud.user import get_all_users
 from models import db_helper
-from schemas.user import UserRead
+from schemas.user import UserRead, UserCreate
 
 router = APIRouter(tags=["users"], prefix="/users")
 
 
 @router.get("/", response_model=list[UserRead])
 async def get_users(
-        session: AsyncSession = Depends(db_helper.session_getter)
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        # session: AsyncSession = Depends(db_helper.session_getter),
 ):
     return await get_all_users(session=session)
+
+
+@router.post("/{user_id}", response_model=UserRead)
+async def create_user(
+        session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+        new_user: UserCreate,
+):
+    user = await create_user(session=session, new_user=new_user)
+    return user
--- app/crud/user.py
+++ app/crud/user.py
@@ -4,9 +4,17 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import User
+from schemas.user import UserCreate
 
 
 async def get_all_users(session: AsyncSession) -> List[User]:
     stmt = select(User).order_by(User.id)
     result = await session.scalars(stmt)
     return list(result.all())
+
+async def create_user(session: AsyncSession, new_user: UserCreate) -> User:
+    user = User(**new_user.model_dump())
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
